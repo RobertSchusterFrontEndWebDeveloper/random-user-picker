94c63252d07e1693f8334fd8dec33831ab350f4b
Rob Schuster <robert.is@icloud.com>
2023-02-07 22:11:33 -0500

    Parsed api and drilled to the result and logged it out

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 const randomFolks = document.querySelector(".random-peeps");
+
+// connect to api to get user list
+const getData = async function () {
+  const usersRequest = await fetch ("https://randomuser.me/api?results=5");
+  const data = await usersRequest.json(); 
+  // console.log(data);  // confirm connection to api
+
+  // output user result
+  const userResults = data.results;
+  console.log(userResults);
+};
+
+getData();
+
