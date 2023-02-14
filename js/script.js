d2dc0d8c723d1fb534ecc9302078c6986d8eb190
Rob Schuster <robert.is@icloud.com>
2023-02-13 20:39:23 -0500

    Add notations

--- js/script.js
+++ js/script.js
@@ -3,12 +3,12 @@ const randomFolks = document.querySelector(".random-peeps");
 // connect to api to get user list
 const getData = async function () {
   const usersRequest = await fetch ("https://randomuser.me/api?results=5");
-  const data = await usersRequest.json(); 
-  // console.log(data);  // confirm connection to api
+  const data = await usersRequest.json(); // parse the api result
+  // console.log(data);  // test out the api output
 
   // output user result
-  const userResults = data.results;
-  // console.log(userResults); 
+  const userResults = data.results; // map the property
+  // console.log(userResults);  // test out the map
   displayUsers(userResults);
 };
 
@@ -16,18 +16,16 @@ getData();
 
  // displayUser function
 const displayUsers = function(userResults) {
-  
-  // First empty the randomFolks constant
-  randomFolks.innerHTML = "";
+  randomFolks.innerHTML = ""; // empty the randomFolks
 
-  // assign random user profile to constants to variables
   for (const user of userResults) {
+    // assign values to users data
     const country = user.location.country; 
     const name = user.name.first;
     const imageUrl = user.picture.medium;
-    const userDiv = document.createElement("div");
+    const userDiv = document.createElement("div"); // create display div
 
-    // set up display of above objects
+    // display div for DOM
     userDiv.innerHTML = `
       <h3>${name}</h3>
       <p>${country}</p>
